7b7624374f3f2ac8f4051cb8624016e9ca4e7c83
s3767649 <48371525+johnazzam@users.noreply.github.com>
2020-02-09 22:11:31 +1100

    Upload python files.

---
+++ text to encryption.py
@@ -0,0 +1,53 @@
+import hashlib
+
+print("What encryption would you like to use?")
+print("0. sha1")
+print("1. sha224")
+print("2. sha256")
+print("3. sha384")
+print("4. sha512")
+print("5. md5")
+print("")
+encType = input('Input your selection')
+print(" ")
+if encType == "0":
+    print('Input text to encrypt')
+    a = input()
+    result = hashlib.sha1(a.encode())
+    print("Your SHA1 encrypted text is: ")
+    print(result.hexdigest())
+
+elif encType == '1':
+    print('Input text to encrypt')
+    i = input()
+    result = hashlib.sha224(i.encode())
+    print("Your SHA224 encrypted text is: ")
+    print(result.hexdigest())
+
+elif encType == '2':
+    print('Input text to encrypt')
+    c = input()
+    result = hashlib.sha256(c.encode())
+    print("Your SHA256 encrypted text is: ")
+    print(result.hexdigest())
+
+elif encType == '3':
+    print('Input text to encrypt')
+    b = input()
+    result = hashlib.sha384(b.encode())
+    print("Your SHA384 encrypted text is: ")
+    print(result.hexdigest())
+
+elif encType == '4':
+    print('Input text to encrypt')
+    d = input()
+    result = hashlib.sha512(d.encode())
+    print("Your SHA512 encrypted text is: ")
+    print(result.hexdigest())
+
+elif encType == '5':
+    print('Input text to encrypt')
+    e = input()
+    result = hashlib.md5(e.encode())
+    print("Your MD5 encrypted text is: ")
+    print(result.hexdigest())
